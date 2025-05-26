33f93f81772f07aca416b63a8a269e109a6b679a
Jak3M <jlw6563@rit.edu>
2025-05-26 13:08:16 -0400

    Basic camera movement & manager
    Adding a basic camera movement

---
+++ Babel/Assets/Scripts/Camera Movement.cs
@@ -0,0 +1,41 @@
+//====== 05/26/25 Jake Wardell ======
+/*
+ *  Camera movement script - Handles player input and applying forces to camera character
+ *  
+ *  Attached to the camera
+ * 
+ * Dependencies: Camera RigidBody
+ * 
+ *  Changelog:
+ *  
+ * 
+ */
+//===================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    Rigidbody2D rb;
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Creates a zeroed out velocity
+        Vector2 move = Vector2.zero;
+
+        //Changes it to the relative vectors
+        if (Input.GetKey(KeyCode.W)) move += Vector2.up;
+        if (Input.GetKey(KeyCode.S)) move += Vector2.down;
+
+        //Sets the velocity
+        rb.velocity = move * speed * Time.deltaTime;
+
+    }
+}
---
+++ Babel/Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public uint floor = 0;
+    public uint money = 0;
+    public uint piety = 0;
+}
