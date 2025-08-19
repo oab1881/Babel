79692e5d99c69a1819074fa483c271d55bae7c81
Owen Beck <112406398+oab1881@users.noreply.github.com>
2025-08-19 14:15:35 -0400

    Added new Census Report stats
    -Added new stats
    --Hooked them all up to stats script so they update live
    -Adjusted the animator so it opens smoothly using a mask

--- Babel/Assets/Scripts/Managers/Stats.cs
+++ Babel/Assets/Scripts/Managers/Stats.cs
@@ -1,14 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
+    //General Stats
     private StatsVal money;
     private StatsVal heresy;
     private StatsVal workerCount;
     private StatsVal engineerCount;
+
+    //Census Stats
+    private StatsVal population;
+    private StatsVal taxes;
+    private StatsVal floorCount;
+    private StatsVal heresyPerMin;
+
+    //Populace Stats
     public StatsVal dogma;
+    public StatsVal culture;
+    public StatsVal power;
+
+    //Census stat text fields
+    [SerializeField]
+    private TextMeshProUGUI populationField;
+    [SerializeField]
+    private TextMeshProUGUI floorsField; 
+    [SerializeField]
+    private TextMeshProUGUI taxesField;
+    [SerializeField]
+    private TextMeshProUGUI heresyPerMinField;
+
+
 
 
     private void Awake()
@@ -17,6 +41,10 @@ public class Stats : MonoBehaviour
         heresy = new StatsVal(0, false, 100);
         engineerCount = new StatsVal(0, false);
         workerCount= new StatsVal(0, false);
+        floorCount = new StatsVal(0, false);
+        taxes = new StatsVal(0, false);
+        population = new StatsVal(0, false);
+        heresyPerMin = new StatsVal(0, false);
     }
 
 
@@ -46,6 +74,39 @@ public class Stats : MonoBehaviour
         set { engineerCount = value; }
     }
 
+    private void Update()
+    {
+        //Count all current floors
+        int currentFloors = FindObjectsOfType<FloorInformation>().Length;
+        floorCount.Modify(currentFloors);
+
+        //Calculate total taxes (sum of goldPerSecond across all floors)
+        float totalTaxes = 0;
+        foreach (GoldGenerator g in FindObjectsOfType<GoldGenerator>())
+        {
+            totalTaxes += g.GoldPerSecond;
+        }
+        taxes.Modify(totalTaxes);
+
+        //Grab heresy per minute from HerecyManager
+        if (HerecyManager.Instance != null)
+            heresyPerMin.Modify(HerecyManager.HeresyAMin);
+
+
+        /// ---- UPDATE STATS HERE ----
+        //Update Floor text
+        if (floorsField != null)
+            floorsField.text = $"{GameManager.FormatNumbers(floorCount.Val)}";
+
+        //Update tax text here
+        if (taxesField != null)
+            taxesField.text = $"{GameManager.FormatNumbers(taxes.Val)}/sec";
+
+        //Update heresyPerMin
+        if (heresyPerMinField != null)
+            heresyPerMinField.text = $"{GameManager.FormatNumbers(heresyPerMin.Val)}/min";
+    }
+
 
     public void ResetStats()
     {
@@ -54,5 +115,9 @@ public class Stats : MonoBehaviour
         workerCount.Modify(0);
         engineerCount.Modify(0);
         dogma.Modify(0);
+        floorCount.Modify(0);
+        taxes.Modify(0);
+        population.Modify(0);
+        heresyPerMin.Modify(0);
     }
 }
